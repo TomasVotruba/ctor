1d05153bff30f3c32de8eed1d42ddfa05a9a749a
Tomas Votruba <tomas.vot@gmail.com>
2026-05-29 18:05:17 +0200

    fix: false-positive with no-arg setters (fixes TomasVotruba/ctor#2) (#5)
    Co-authored-by: fix-issue-bot <fix-issue-bot@localhost>

---
+++ tests/Rules/NewOverSettersRule/Source/SomeTimerObject.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TomasVotruba\Ctor\Tests\Rules\NewOverSettersRule\Source;
+
+class SomeTimerObject
+{
+    public function setStartTime()
+    {
+    }
+
+    public function setEndTime()
+    {
+    }
+}
